04575309eed2ffc0222bf56bb28655b22c4421aa
Privitera Stefano Salvatore <s.privitera@campus.unimib.it>
2026-01-31 21:51:05 +0100

    initialize Kotlin Multiplatform project structure with basic configurations and platform-specific implementations

---
+++ composeApp/src/commonTest/kotlin/org/stefanoprivitera/klock/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.stefanoprivitera.klock
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
