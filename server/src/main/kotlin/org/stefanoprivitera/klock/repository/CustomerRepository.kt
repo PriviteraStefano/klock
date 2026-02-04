5fa3a17462c9cf75f884d3cc84568775ca3bdaab
Privitera Stefano Salvatore <s.privitera@campus.unimib.it>
2026-02-04 23:26:53 +0100

    refactor CustomerRepository interface to return Uuid and Customer objects instead of String; improve type safety and clarity

--- server/src/main/kotlin/org/stefanoprivitera/klock/repository/CustomerRepository.kt
+++ server/src/main/kotlin/org/stefanoprivitera/klock/repository/CustomerRepository.kt
@@ -1,14 +1,15 @@
 package org.stefanoprivitera.klock.repository
 
+import org.stefanoprivitera.klock.domain.Customer
 import org.stefanoprivitera.klock.domain.CustomerRequest
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 @OptIn(ExperimentalUuidApi::class)
 interface CustomerRepository {
-    fun create(customer: CustomerRequest.Create): String
-    fun findById(id: Uuid): String?
-    fun findAll(filter: CustomerRequest.Filter): List<String>
+    fun create(customer: CustomerRequest.Create): Uuid
+    fun findById(id: Uuid): Customer?
+    fun findAll(filter: CustomerRequest.Filter): List<Customer>
     fun update(customer: CustomerRequest.Update): Int
     fun deleteById(id: Uuid): Int
 }
